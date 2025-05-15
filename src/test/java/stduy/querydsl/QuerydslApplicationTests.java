b2d9ed63047290ef12739b519b09b1b78b58fcfc
bangddong <gusehdqkd@naver.com>
2025-05-15 20:58:36 +0900

    feat : 기본 엔티티 생성 및 동작 확인

--- src/test/java/stduy/querydsl/QuerydslApplicationTests.java
+++ src/test/java/stduy/querydsl/QuerydslApplicationTests.java
@@ -1,43 +1,11 @@
 package stduy.querydsl;
 
-import static org.assertj.core.api.Assertions.*;
-
-import org.assertj.core.api.Assertions;
-import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.annotation.Commit;
 import org.springframework.transaction.annotation.Transactional;
-
-import com.querydsl.jpa.impl.JPAQueryFactory;
-
-import jakarta.persistence.EntityManager;
-import jakarta.persistence.PersistenceContext;
-import stduy.querydsl.entity.Hello;
-import stduy.querydsl.entity.QHello;
 
 @SpringBootTest
 @Transactional
 @Commit
 class QuerydslApplicationTests {
-
-	@Autowired
-	EntityManager em;
-
-	@Test
-	void contextLoads() {
-		Hello hello = new Hello();
-		em.persist(hello);
-
-		JPAQueryFactory query = new JPAQueryFactory(em);
-		QHello qHello = QHello.hello;
-
-		Hello result = query
-			.selectFrom(qHello)
-			.fetchOne();
-
-		assertThat(result).isEqualTo(hello);
-		assertThat(result.getId()).isEqualTo(hello.getId());
-	}
-
 }
