7431c948f31fe1eeb6b5e946a28b5365ab650cfb
bangddong <gusehdqkd@naver.com>
2025-05-27 21:51:13 +0900

    조인 대상 필터링, 연관관계 없는 엔티티 외부 조인

--- src/test/java/stduy/querydsl/QuerydslBasicTest.java
+++ src/test/java/stduy/querydsl/QuerydslBasicTest.java
@@ -288,4 +288,42 @@ public class QuerydslBasicTest {
 			.containsExactly("teamA", "teamB");
 	}
 
+	/**
+	 * 회원과 팀을 조인하면서, 팀 이름이 teamA인 팀만 조인, 회원은 모두 조회
+	 * JPQL : SELECT m, t FROM Member m LEFT JOIN m.team t WHERE t.name = 'teamA'
+	 * SQL : SELECT m.*, t.* FROM member m LEFT JOIN team t ON t.team_id = m.team_id WHERE t.name = 'teamA'
+	 */
+	@Test
+	public void join_on_filtering() throws Exception {
+		List<Tuple> result = queryFactory
+			.select(member, team)
+			.from(member)
+			.leftJoin(member.team, team).on(team.name.eq("teamA"))
+			.fetch();
+
+		for (Tuple tuple : result) {
+			System.out.println("tuple = " + tuple);
+		}
+	}
+
+	/**
+	 * 연관관계 없는 엔티티 외부 조인
+	 * 회원의 이름이 팀 이름과 같은 대상 외부 조인
+	 */
+	@Test
+	public void join_on_no_relation() throws Exception {
+		em.persist(new Member("teamA"));
+		em.persist(new Member("teamB"));
+
+		List<Tuple> result = queryFactory
+			.select(member, team)
+			.from(member)
+			.leftJoin(team).on(member.username.eq(team.name))
+			.fetch();
+
+		for (Tuple tuple : result) {
+			System.out.println("tuple = " + tuple);
+		}
+	}
+
 }
